c76221836d4824f624ab414a0b6c9b6d5c0d6d54
ML78-AI <58592999+ML78-AI@users.noreply.github.com>
2021-09-04 11:00:21 +0200

    Update detection.go

--- copypaste/detection.go
+++ copypaste/detection.go
@@ -5,14 +5,12 @@ import (
 	"path"
 )
 
-// LocationUSB : retourne le chemin avec le nom de la session d'utilisateur pour accéder aux clés USB
 func LocationUSB(pathMedia string) string {
 	listUser, err := ioutil.ReadDir(pathMedia)
 	check(err)
 	return path.Join(pathMedia, listUser[0].Name())
 }
 
-// DetectionUSB : renvoie les dossiers à copier coller
 func DetectionUSB(keyPath string) []string {
 	var ListKeyPaths []string
 	KeyLists, err := ioutil.ReadDir(keyPath)
